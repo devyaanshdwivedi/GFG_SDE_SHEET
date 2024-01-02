48a76dbe260ae92e720831e5421440d924363d86
Devyaansh Dwivedi <91078095+devyaanshdwivedi@users.noreply.github.com>
2024-01-02 08:50:32 +0530

    Add files via upload

---
+++ Implement Atoi.py
@@ -0,0 +1,11 @@
+#User function template for Python
+
+class Solution:
+    # your task is to complete this function
+    # function should return an integer
+    def atoi(self,string):
+        # Code here
+        try:
+            return int(string)
+        except:
+            return-1
